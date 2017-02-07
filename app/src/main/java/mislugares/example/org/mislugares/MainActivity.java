e51b7bc668d9babfe268d42e6ee77b9a6a69cedc
Yudy Quintero <yudy.quintero@udea.edu.co>
2017-02-07 14:15:50 -0500

    agrega nuevas funcionalidades

--- app/src/main/java/mislugares/example/org/mislugares/MainActivity.java
+++ app/src/main/java/mislugares/example/org/mislugares/MainActivity.java
@@ -1,7 +1,9 @@
 package mislugares.example.org.mislugares;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
@@ -10,8 +12,11 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static Lugares lugares = new LugaresVector();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,12 @@ public class MainActivity extends AppCompatActivity {
         bSalir.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 finish();
+            }
+        });
+        Button bMostrar=(Button)findViewById(R.id.button01);
+        bMostrar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                mostrarPreferencias(view);
             }
         });
 
@@ -47,9 +58,27 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this, AcercaDeActivity.class);
         startActivity(i);
     }
-    public void salir(View view){
-        finish();
+
+    public void lanzarPreferencias(View view){
+        Intent i = new Intent(this, PreferenciasActivity.class);
+        startActivity(i);
     }
+
+    public void lanzarVistaLugar(View view){
+        Intent i = new Intent(this, VistaLugarActivity.class);
+        i.putExtra("id", (long)0);
+        startActivity(i);
+    }
+
+    public void mostrarPreferencias(View view){
+        SharedPreferences pref =
+                PreferenceManager.getDefaultSharedPreferences(this);
+        String s = "notificaciones: "+ pref.getBoolean("notificaciones",true)
+                +", máximo a listar: " + pref.getString("maximo","?");
+        Toast.makeText(this, s, Toast.LENGTH_SHORT).show();
+    }
+
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -60,13 +89,25 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            lanzarPreferencias(null);
             return true;
         }
 
-        if (id == R.id. acercaDe) {
+        if (id == R.id.acercaDe) {
             lanzarAcercaDe(null);
             return true;
         }
+
+        if(id == R.id.menu_buscar) {
+            lanzarVistaLugar(null);
+            return true;
+        }
+
         return super.onOptionsItemSelected(item);
     }
+
+    public void salir(View view){
+        finish();
+    }
+
 }
